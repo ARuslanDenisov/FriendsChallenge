e8d19e29909d9374daf7e3f2b01a781354ff5d99
ARuslanDenisov <angess47@gmail.com>
2024-06-27 18:10:47 +0700

    Initial Commit

---
+++ FriendsChallenge/FriendsChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsChallengeApp.swift
+//  FriendsChallenge
+//
+//  Created by Руслан on 27.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct FriendsChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
